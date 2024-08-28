171e7a9f973ace95a7e19135ed4e56872c58961a
Gonzalo Ilhuicatzi <g.ilhuicatzi@gmail.com>
2024-08-28 03:16:17 -0600

    quizz topic x4  added

---
+++ src/lib/quizz/AlgebraQuizzes.ts
@@ -0,0 +1,162 @@
+// export const Example = [
+//     {
+//       id: 1,
+//       title: "Pregunta 1",
+//       quizz_id: "quizz1",
+//       description:
+//         "Una caja con lápices de colores contiene 20 paquetes, los que a su vez tienen 12 lápices de colores cada uno, si hay 25 cajas. ¿Cuántos lápices se tienen en total?",
+//       options: [
+//         { id: "1-r1", value: "240", item: "A", color: "bg-teal-600" },
+//         { id: "1-r2", value: "300", item: "B", color: "bg-rose-600" },
+//         { id: "1-r3", value: "3000", item: "C", color: "bg-violet-600" },
+//         { id: "1-r4", value: "6000", item: "D", color: "bg-orange-600" },
+//       ],
+//       correctAnswer: "1-r2",
+//     }, ...
+//   ];
+
+export const terminosSemejantes = [
+    {
+        id: 1,
+        title: "Pregunta 1",
+        quizz_id: "quizz1",
+        description:
+            "Simplifica la expresión $3x + 2y - 5x + 4y$",
+        options: [
+            { id: "1-r1", value: "$2x + 6y$", item: "A", color: "bg-teal-600" },
+            { id: "1-r2", value: "$-2x + 6y$", item: "B", color: "bg-rose-600" },
+            { id: "1-r3", value: "$2x - 6y$", item: "C", color: "bg-violet-600" },
+            { id: "1-r4", value: "$-2x - 6y$", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "1-r1",
+    },
+    {
+        id: 2,
+        title: "Pregunta 2",
+        quizz_id: "quizz2",
+        description:
+            "Simplifica la expresión $5a - 3b + 2a - 4b$",
+        options: [
+            { id: "2-r1", value: "$7a - 7b$", item: "A", color: "bg-teal-600" },
+            { id: "2-r2", value: "$7a + 7b$", item: "B", color: "bg-rose-600" },
+            { id: "2-r3", value: "$3a - 7b$", item: "C", color: "bg-violet-600" },
+            { id: "2-r4", value: "$3a + 7b$", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "2-r1",
+    },
+    {
+        id: 3,
+        title: "Pregunta 3",
+        quizz_id: "quizz3",
+        description:
+            "Simplifica la expresión $6x - 3y - 2x + 5y$",
+        options: [
+            { id: "3-r1", value: "$4x + 2y$", item: "A", color: "bg-teal-600" },
+            { id: "3-r2", value: "$4x - 2y$", item: "B", color: "bg-rose-600" },
+            { id: "3-r3", value: "$4x + 8y$", item: "C", color: "bg-violet-600" },
+            { id: "3-r4", value: "$4x - 8y$", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "3-r1",
+    },
+    {
+        id: 4,
+        title: "Pregunta 4",
+        quizz_id: "quizz4",
+        description:
+            "Simplifica la expresión $3a - 2b + 4a - 3b$",
+        options: [
+            { id: "4-r1", value: "$7a - 5b$", item: "A", color: "bg-teal-600" },
+            { id: "4-r2", value: "$7a + 5b$", item: "B", color: "bg-rose-600" },
+            { id: "4-r3", value: "$5a - 7b$", item: "C", color: "bg-violet-600" },
+            { id: "4-r4", value: "$5a + 7b$", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "4-r1",
+    },
+    {
+        id: 5,
+        title: "Pregunta 5",
+        quizz_id: "quizz5",
+        description:
+            "Simplifica la expresión $4x - 3y + 2x - 5y$",
+        options: [
+            { id: "5-r1", value: "$6x - 8y$", item: "A", color: "bg-teal-600" },
+            { id: "5-r2", value: "$6x + 8y$", item: "B", color: "bg-rose-600" },
+            { id: "5-r3", value: "$2x - 8y$", item: "C", color: "bg-violet-600" },
+            { id: "5-r4", value: "$2x + 8y$", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "5-r1",
+    },
+];
+
+export const multiplicacionMonomios = [
+    {
+        id: 1,
+        title: "Pregunta 1",
+        quizz_id: "quizz1",
+        description:
+            "Multiplica los monomios $(3x)(2x)$",
+        options: [
+            { id: "1-r1", value: "$6x^2$", item: "A", color: "bg-teal-600" },
+            { id: "1-r2", value: "$5x^2$", item: "B", color: "bg-rose-600" },
+            { id: "1-r3", value: "$6x$", item: "C", color: "bg-violet-600" },
+            { id: "1-r4", value: "$5x$", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "1-r1",
+    },
+    {
+        id: 2,
+        title: "Pregunta 2",
+        quizz_id: "quizz2",
+        description:
+            "Multiplica los monomios $(5a) \\cdot (3a)$",
+        options: [
+            { id: "2-r1", value: "$15a^2$", item: "A", color: "bg-teal-600" },
+            { id: "2-r2", value: "$8a^2$", item: "B", color: "bg-rose-600" },
+            { id: "2-r3", value: "$15a$", item: "C", color: "bg-violet-600" },
+            { id: "2-r4", value: "$8a$", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "2-r1",
+    },
+    {
+        id: 3,
+        title: "Pregunta 3",
+        quizz_id: "quizz3",
+        description:
+            "Multiplica los monomios $(6x)(3x)$ ",
+        options: [
+            { id: "3-r1", value: "$18x^2$", item: "A", color: "bg-teal-600" },
+            { id: "3-r2", value: "$9x^2$", item: "B", color: "bg-rose-600" },
+            { id: "3-r3", value: "$18^2$", item: "C", color: "bg-violet-600" },
+            { id: "3-r4", value: "$9^2$", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "3-r1",
+    },
+    {
+        id: 4,
+        title: "Pregunta 4",
+        quizz_id: "quizz4",
+        description:
+            "Multiplica los monomios $(3a)(4a)$",
+        options: [
+            { id: "4-r1", value: "$12a^2$", item: "A", color: "bg-teal-600" },
+            { id: "4-r2", value: "$7a^2$", item: "B", color: "bg-rose-600" },
+            { id: "4-r3", value: "$12a$", item: "C", color: "bg-violet-600" },
+            { id: "4-r4", value: "$7a$", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "4-r1",
+    },
+    {
+        id: 5,
+        title: "Pregunta 5",
+        quizz_id: "quizz5",
+        description:
+            "Multiplica los monomios $(4x)(2x)$",
+        options: [
+            { id: "5-r1", value: "$8x^2$", item: "A", color: "bg-teal-600" },
+            { id: "5-r2", value: "$6x^2$", item: "B", color: "bg-rose-600" },
+            { id: "5-r3", value: "$8x$", item: "C", color: "bg-violet-600" },
+            { id: "5-r4", value: "$6x$", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "5-r1",
+    },
+];
---
+++ src/lib/quizz/AritmeticaQuizzes.ts
@@ -0,0 +1,162 @@
+// export const Example = [
+//     {
+//       id: 1,
+//       title: "Pregunta 1",
+//       quizz_id: "quizz1",
+//       description:
+//         "Una caja con lápices de colores contiene 20 paquetes, los que a su vez tienen 12 lápices de colores cada uno, si hay 25 cajas. ¿Cuántos lápices se tienen en total?",
+//       options: [
+//         { id: "1-r1", value: "$ \\dfrac{}{} $", item: "A", color: "bg-teal-600" },
+//         { id: "1-r2", value: "$ \\dfrac{}{} $", item: "B", color: "bg-rose-600" },
+//         { id: "1-r3", value: "$ \\dfrac{}{} $", item: "C", color: "bg-violet-600" },
+//         { id: "1-r4", value: "$ \\dfrac{}{} $", item: "D", color: "bg-orange-600" },
+//       ],
+//       correctAnswer: "1-r2",
+//     }, ...
+//   ];
+
+export const SumaFracciones = [
+    {
+        id: 1,
+        title: "Pregunta 1",
+        quizz_id: "quizz1",
+        description:
+            "¿Cuál es la suma de $$ \\dfrac{1}{3} + \\dfrac{1}{4} $?",
+        options: [
+            { id: "1-r1", value: "$ \\dfrac{2}{7} $", item: "A", color: "bg-teal-600" },
+            { id: "1-r2", value: "$ \\dfrac{1}{12} $", item: "B", color: "bg-rose-600" },
+            { id: "1-r3", value: "$ \\dfrac{2}{12} $", item: "C", color: "bg-violet-600" },
+            { id: "1-r4", value: "$ \\dfrac{7}{12} $", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "1-r4",
+    },
+    {
+        id: 2,
+        title: "Pregunta 2",
+        quizz_id: "quizz2",
+        description:
+            "¿Cuál es la suma de $$ \\dfrac{1}{2} + \\dfrac{1}{3} $?",
+        options: [
+            { id: "2-r1", value: "$ \\dfrac{5}{6} $", item: "A", color: "bg-teal-600" },
+            { id: "2-r2", value: "$ \\dfrac{2}{5} $", item: "B", color: "bg-rose-600" },
+            { id: "2-r3", value: "$ \\dfrac{2}{6} $", item: "C", color: "bg-violet-600" },
+            { id: "2-r4", value: "$ \\dfrac{1}{5} $", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "2-r1",
+    },
+    {
+        id: 3,
+        title: "Pregunta 3",
+        quizz_id: "quizz3",
+        description:
+            "¿Cuál es la suma de $$ \\dfrac{1}{4} + \\dfrac{1}{5} $?",
+        options: [
+            { id: "3-r1", value: "$ \\dfrac{9}{20} $", item: "A", color: "bg-teal-600" },
+            { id: "3-r2", value: "$ \\dfrac{2}{9} $", item: "B", color: "bg-rose-600" },
+            { id: "3-r3", value: "$ \\dfrac{1}{9} $", item: "C", color: "bg-violet-600" },
+            { id: "3-r4", value: "$ \\dfrac{2}{20} $", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "3-r1",
+    },
+    {
+        id: 4,
+        title: "Pregunta 4",
+        quizz_id: "quizz4",
+        description:
+            "¿Cuál es la suma de $$ \\dfrac{1}{5} + \\dfrac{1}{6} $?",
+        options: [
+            { id: "4-r1", value: "$ \\dfrac{11}{30} $", item: "A", color: "bg-teal-600" },
+            { id: "4-r2", value: "$ \\dfrac{2}{30} $", item: "B", color: "bg-rose-600" },
+            { id: "4-r3", value: "$ \\dfrac{2}{11} $", item: "C", color: "bg-violet-600" },
+            { id: "4-r4", value: "$ \\dfrac{1}{30} $", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "4-r1",
+    },
+    {
+        id: 5,
+        title: "Pregunta 5",
+        quizz_id: "quizz5",
+        description:
+            "¿Cuál es la suma de $$ \\dfrac{1}{6} + \\dfrac{1}{7} $?",
+        options: [
+            { id: "5-r1", value: "$ \\dfrac{13}{42} $", item: "A", color: "bg-teal-600" },
+            { id: "5-r2", value: "$ \\dfrac{2}{42} $", item: "B", color: "bg-rose-600" },
+            { id: "5-r3", value: "$ \\dfrac{2}{13} $", item: "C", color: "bg-violet-600" },
+            { id: "5-r4", value: "$ \\dfrac{1}{42} $", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "5-r1",
+    },
+];
+
+export const RestaFracciones = [
+    {
+        id: 1,
+        title: "Pregunta 1",
+        quizz_id: "quizz1",
+        description:
+            "¿Cuál es la resta de $$ \\dfrac{1}{3} - \\dfrac{1}{4} $?",
+        options: [
+            { id: "1-r1", value: "$ \\dfrac{1}{12} $", item: "A", color: "bg-teal-600" },
+            { id: "1-r2", value: "$ \\dfrac{1}{7} $", item: "B", color: "bg-rose-600" },
+            { id: "1-r3", value: "$ \\dfrac{3}{4} $", item: "C", color: "bg-violet-600" },
+            { id: "1-r4", value: "$ \\dfrac{4}{3} $", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "1-r2",
+    },
+    {
+        id: 2,
+        title: "Pregunta 2",
+        quizz_id: "quizz2",
+        description:
+            "¿Cuál es la resta de $$ \\dfrac{1}{2} - \\dfrac{1}{3} $?",
+        options: [
+            { id: "2-r1", value: "$ \\dfrac{1}{6} $", item: "A", color: "bg-teal-600" },
+            { id: "2-r2", value: "$ \\dfrac{1}{5} $", item: "B", color: "bg-rose-600" },
+            { id: "2-r3", value: "$ \\dfrac{2}{3} $", item: "C", color: "bg-violet-600" },
+            { id: "2-r4", value: "$ \\dfrac{3}{2} $", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "2-r1",
+    },
+    {
+        id: 3,
+        title: "Pregunta 3",
+        quizz_id: "quizz3",
+        description:
+            "¿Cuál es la resta de $$ \\dfrac{1}{4} - \\dfrac{1}{5} $?",
+        options: [
+            { id: "3-r1", value: "$ \\dfrac{1}{20} $", item: "A", color: "bg-teal-600" },
+            { id: "3-r2", value: "$ \\dfrac{1}{9} $", item: "B", color: "bg-rose-600" },
+            { id: "3-r3", value: "$ \\dfrac{4}{5} $", item: "C", color: "bg-violet-600" },
+            { id: "3-r4", value: "$ \\dfrac{5}{4} $", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "3-r1",
+    },
+    {
+        id: 4,
+        title: "Pregunta 4",
+        quizz_id: "quizz4",
+        description:
+            "¿Cuál es la resta de $$ \\dfrac{1}{5} - \\dfrac{1}{6} $?",
+        options: [
+            { id: "4-r1", value: "$ \\dfrac{1}{30} $", item: "A", color: "bg-teal-600" },
+            { id: "4-r2", value: "$ \\dfrac{1}{11} $", item: "B", color: "bg-rose-600" },
+            { id: "4-r3", value: "$ \\dfrac{5}{6} $", item: "C", color: "bg-violet-600" },
+            { id: "4-r4", value: "$ \\dfrac{6}{5} $", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "4-r1",
+    },
+    {
+        id: 5,
+        title: "Pregunta 5",
+        quizz_id: "quizz5",
+        description:
+            "¿Cuál es la resta de $$ \\dfrac{1}{6} - \\dfrac{1}{7} $?",
+        options: [
+            { id: "5-r1", value: "$ \\dfrac{1}{42} $", item: "A", color: "bg-teal-600" },
+            { id: "5-r2", value: "$ \\dfrac{1}{13} $", item: "B", color: "bg-rose-600" },
+            { id: "5-r3", value: "$ \\dfrac{6}{7} $", item: "C", color: "bg-violet-600" },
+            { id: "5-r4", value: "$ \\dfrac{7}{6} $", item: "D", color: "bg-orange-600" },
+        ],
+        correctAnswer: "5-r1",
+    },
+];
